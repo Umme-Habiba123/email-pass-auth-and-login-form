83ae08fe5daaa87b8a77ba8f8a8616f51abff09a
umme-habiba123 <habibaislammim@gmail.com>
2025-04-27 21:09:07 +0600

    project and route setup

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+  
+        
+     
+      <h1 className='text-5xl text-center'>Explore Email Password Authentication</h1>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/Layout/Root.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Outlet } from 'react-router';
+
+const Root = () => {
+    return (
+        <div>
+
+            <Outlet></Outlet>
+            
+        </div>
+    );
+};
+
+export default Root;
